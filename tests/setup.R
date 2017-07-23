62b38805e683f62494fdbe577b59ce572da44332
Anthony Damico <ajdamico@gmail.com>
2017-07-23 02:08:27 -0400

    sampling to keep build within restrictions

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 library(lodown)
-lodown( "faers" , output_dir = file.path( getwd() ) )
+
+faers_cat <-
+	get_catalog( "faers" ,
+		output_dir = file.path( getwd() ) )
+
+# sample 75% of the records
+which_records <- sample( seq( nrow( faers_cat ) ) , round( nrow( faers_cat ) * 0.75 ) )
+
+# always sample year == 2016
+faers_cat <- unique( faers_cat[ which_records , ] , subset( faers_cat , year == 2016 ) )
+
+lodown( "faers" , faers_cat )
