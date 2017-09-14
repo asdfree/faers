9a12e4a987c967d88ee64a6eb5e0d1bdcea4cac6
Anthony Damico <ajdamico@gmail.com>
2017-09-14 10:56:58 -0400

    faers 50% pnad 25%

--- tests/setup.R
+++ tests/setup.R
@@ -6,8 +6,8 @@ faers_cat <-
 	get_catalog( "faers" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 75% of the records
-which_records <- sample( seq( nrow( faers_cat ) ) , round( nrow( faers_cat ) * 0.75 ) )
+# sample 50% of the records
+which_records <- sample( seq( nrow( faers_cat ) ) , round( nrow( faers_cat ) * 0.50 ) )
 
 # always sample year == 2016
 faers_cat <- unique( rbind( faers_cat[ which_records , ] , subset( faers_cat , year == 2016 ) ) )
