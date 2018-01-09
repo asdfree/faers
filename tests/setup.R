8fac54e9ce32998f083b140ee9fe7231aaf1bb15
Anthony Damico <ajdamico@gmail.com>
2018-01-09 16:04:37 -0500

    faers

--- tests/setup.R
+++ tests/setup.R
@@ -3,7 +3,7 @@ if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 library(lodown)
 this_sample_break <- Sys.getenv( "this_sample_break" )
 faers_cat <- get_catalog( "faers" , output_dir = file.path( getwd() ) )
-record_categories <- ceiling( seq( nrow( faers_cat ) ) / ceiling( nrow( faers_cat ) / 8 ) )
+record_categories <- ceiling( seq( nrow( faers_cat ) ) / ceiling( nrow( faers_cat ) / 7 ) )
 faers_cat <- faers_cat[ record_categories == this_sample_break , ]
 lodown( "faers" , faers_cat )
 if( any( faers_cat$year == 2016 & faers_cat$quarter == 4 ) ){
